230eed2af7b36a94773d9a3261e563e1b94120ef
afirstenberg <github@addventure.com>
2022-04-28 17:37:09 -0400

    Illustration of how to make things even simpler if the configuration is managed by mcms

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,39 @@
+import Multivocal from 'multivocal'
+import Util from 'multivocal/lib/util'
 
-const Color = require('./color.js');
-Color.init();
+async function buildEnvColor( env ){
+  // What color did the user mention
+  const color = Util.objPath( env, 'Parameter/color');
 
-const Multivocal = require('multivocal');
+  // What have they previously told us their favorite color was?
+  let favoriteColor = Util.objPath( env, 'User/State/favoriteColor' );
+  if( !favoriteColor && color ){
+    // They didn't have a favorite color set, so remember this
+    favoriteColor = color;
+    Util.setObjPath( env, 'User/State/favoriteColor', favoriteColor );
+  }
+
+  // Store these in the environment
+  env.color = color;
+  env.favoriteColor = favoriteColor;
+
+  return env
+}
+
+async function handleColor( env ){
+  // Set our outent name based on the color they sent
+  const color = env.color;
+  Util.setObjPath( env, 'Outent', `Outent.color.${color}` );
+
+  return Multivocal.handleDefault( env );
+}
+
+// Register functions
+Multivocal.addBuilder( buildEnvColor );
+Multivocal.addActionHandler( 'color', handleColor );
+
+// Load our configuration from a Firestore db in the current project
+// at a default location
+new Multivocal.Config.Firestore()
+
 exports.webhook = Multivocal.processFirebaseWebhook;
